1bbd46b793afc02402a66e792c2ba27dfdffb844
Daniel Nicolás Madera <dmadera@waypoint.la>
2023-10-11 10:43:14 -0300

    chapter 64 - add Agregate rating

--- models/Review.js
+++ models/Review.js
@@ -40,4 +40,46 @@ const ReviewSchema = new mongoose.Schema({
 ReviewSchema.index({ bootcamp: 1, user:1 }, { unique: true })
 
 
+//static method to get avg cost of course tuition
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+    const result = await this.aggregate([{
+        $match: { bootcamp: bootcampId}
+    }, 
+    {
+        $group: {
+            _id: '$bootcamp',
+            averageRating: { $avg: '$rating'}
+        }
+    }
+
+    ]);
+
+    try {        
+
+        let averageRating = result?.[0]?.averageRating ?? 0;        
+        const modelBootcamp = this.model('Bootcamp')
+        await modelBootcamp.findByIdAndUpdate(bootcampId ,{
+            averageRating: averageRating
+        })
+    } catch (error) {
+        console.error(error);
+    }
+    
+
+}
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function() {
+    this.constructor.getAverageRating(this.bootcamp)
+  });
+
+
+//
+ReviewSchema.pre('deleteOne', { document: true }, function() {    
+    this.constructor.getAverageRating(this.bootcamp)
+  })
+
+
+
+
 module.exports = mongoose.model('Review', ReviewSchema);
